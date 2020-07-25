c8c7ef7f363a1451c4ccd41a2df44fb53154e668
Golehm <grzegorz.gorski@yahoo.pl>
2020-07-25 12:07:00 +0200

    updating event attendance UI screens

--- src/main/webapp/app/entities/rfb-event-attendance/rfb-event-attendance.route.ts
+++ src/main/webapp/app/entities/rfb-event-attendance/rfb-event-attendance.route.ts
@@ -40,7 +40,7 @@ export const rfbEventAttendanceRoute: Routes = [
     component: RfbEventAttendanceComponent,
     data: {
       authorities: [Authority.USER],
-      pageTitle: 'RfbEventAttendances',
+      pageTitle: 'Event Attendances',
     },
     canActivate: [UserRouteAccessService],
   },
@@ -52,7 +52,7 @@ export const rfbEventAttendanceRoute: Routes = [
     },
     data: {
       authorities: [Authority.USER],
-      pageTitle: 'RfbEventAttendances',
+      pageTitle: 'Event Attendances',
     },
     canActivate: [UserRouteAccessService],
   },
@@ -64,7 +64,7 @@ export const rfbEventAttendanceRoute: Routes = [
     },
     data: {
       authorities: [Authority.USER],
-      pageTitle: 'RfbEventAttendances',
+      pageTitle: 'Event Attendances',
     },
     canActivate: [UserRouteAccessService],
   },
@@ -76,7 +76,7 @@ export const rfbEventAttendanceRoute: Routes = [
     },
     data: {
       authorities: [Authority.USER],
-      pageTitle: 'RfbEventAttendances',
+      pageTitle: 'Event Attendances',
     },
     canActivate: [UserRouteAccessService],
   },
